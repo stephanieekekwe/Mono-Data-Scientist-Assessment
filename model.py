5b0f92f0ed29f3a5cbdea3530488d33003c02209
stephanieekekwe <stephchisom@gmail.com>
2022-08-06 21:35:10 +0100

    changes

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def model(train_data, transactions_data):
-    model = KMeans(n_clusters=10, init='k-means++',
+    model = KMeans(n_clusters=15, init='k-means++',
                    n_init=100, random_state=52,  max_iter=300)
     model.fit_predict(train_data)
 
