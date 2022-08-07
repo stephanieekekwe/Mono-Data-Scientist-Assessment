abb74a9088dc2d05dbcb9034c07e80367b509edc
stephanieekekwe <stephchisom@gmail.com>
2022-08-07 22:10:00 +0100

    model changes made

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def model(train_data, transactions_data):
     model = KMeans(n_clusters=15, init='k-means++',
-                   n_init=100, random_state=52,  max_iter=300)
+                   n_init=50, random_state=5,  max_iter=50)
     model.fit_predict(train_data)
 
     transactions_data['group'] = model.labels_
